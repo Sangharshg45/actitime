5f2c5b8ccc45de58612f1df80752676fc91be73f
DELL <DELL@DESKTOP-NFAVFHB>
2023-01-18 14:49:07 +0530

    Updated

--- Test1.java
+++ Test1.java
@@ -6,14 +6,10 @@ public class Test1
 {
 	public static void main(String[] args) 
 	{
-		// System.setProperty("webdeiver.chrome.driver","./drivers/chromedriver.exe");
+		
 		ChromeDriver driver=new ChromeDriver();
-		//driver.manage().window().maximize();
+		driver.manage().window().maximize();
 		driver.get("https://demo.actitime.com/login.do");
-		String Qspider = driver.getTitle();
-		String Qspider1 = driver.getCurrentUrl();
-		System.out.println(Qspider);
-		System.out.println(Qspider1);
 		
 	}
 
